efd247f41be2782462a926c13a9fc8cc4dad0cb4
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-18 12:57:11 +0530

    Player Reaches exactly to 100

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,35 +1,49 @@
 package bridgeLabz.Development.SnakeAndLadder;
 
 class Game{
-	//player at start position 0
+//player at start position 0
 		public static int position=0;
 		public static int diceNumber;
 		public static int moment;
 		public static final int ladder=1;
 		public static final int snake=2;
 		
-		//Using Random to get a Random of a dice from 1 to 6 
-		//Checking for the position whether its a ladder, snake or no play
-		//Repeat till the Player reaches the winning position 100.
+//Using Random to get a Random of a dice from 1 to 6 
+//Checking for the position whether its a ladder, snake or no play
+//Repeat till the Player reaches the winning position 100.
+		
 		public void checkMoment() {
 			while(position<100) {
 				diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
 		    	System.out.println("Die number is : "+diceNumber);
 		    moment=(int)(Math.floor(Math.random()*10)%3);
 		    switch(moment) {
+		    
 		    case ladder:
-		    	position+=diceNumber;
-		    	System.out.println("your are at ladder climb up : " +diceNumber);
+//if the player position go above 100, the player stays in the same previous position till the player gets exact 100 
+		    	
+		    	int i=position+=diceNumber;
+		    	if (i>100) {
+		    		position=i-diceNumber;
+			    	System.out.println("You are exceeding the winning position, please stay at the same position");
+		    	}
+		    	else {
+		    		position=i;
+			    	System.out.println("Ladder : " +diceNumber +" steps forward ");
+
+		    	}	
 		    	break;
+		    	
 		    case snake:
 		    	position-=diceNumber;
-		    	System.out.println("Snake bites, move back to the tail : " +diceNumber);
+		    	System.out.println("Snake bite : " +diceNumber +" steps backward ");
 		    	break;
+		    	
 		    default:
 		    	System.out.println("Stay at the same position");
 		    } 
+//In case the player position moves below 0, then the player restarts from 0
 		    
-		    //In case the player position moves below 0, then the player restarts from 0
 		    if(position<0)
 		    	position=0;
 		    }
