7688e0d319b78acd19d93ccc2883e5eaac6e69fc
Janga Rajashekhar Reddy <102745786+Rajashekhar8988@users.noreply.github.com>
2022-09-18 10:52:21 +0530

    Update SnakeAndLadderGame.java

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,6 +1,7 @@
 package bridgeLabz.Development.SnakeAndLadder;
 
 public class SnakeAndLadderGame {
+	//player at start position 0
 	static int position=0;
 	public static void main(String[] args) {
 		System.out.println("position of the player : " + position);
