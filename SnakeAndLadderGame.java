004434d0d2671fd43ed9118c2b69aff01cf87bb7
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-18 10:50:12 +0530

    Random of a dice

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,8 +1,15 @@
 package bridgeLabz.Development.SnakeAndLadder;
 
 public class SnakeAndLadderGame {
+	
+	//player at start position 0
 	static int position=0;
+	
+	 //Using Random to get a Random of a dice from 1 to 6 
+	static int diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
 	public static void main(String[] args) {
 		System.out.println("position of the player : " + position);
+		System.out.println("Random number of Dice is : " + diceNumber);
+
 	}
 }
