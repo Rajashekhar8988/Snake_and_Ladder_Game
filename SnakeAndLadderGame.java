d3450003986e36a54f113826cb21c690df1db115
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-18 10:29:14 +0530

    Player at position 0

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,8 @@
+package bridgeLabz.Development.SnakeAndLadder;
+
+public class SnakeAndLadderGame {
+	static int position=0;
+	public static void main(String[] args) {
+		System.out.println("position of the player : " + position);
+	}
+}
