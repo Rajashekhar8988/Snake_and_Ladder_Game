ce480f5579e7bc4a97820bc71aeb8fffdd1edf23
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-18 11:22:37 +0530

    To check the position of the player

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,15 +1,41 @@
 package bridgeLabz.Development.SnakeAndLadder;
 
-public class SnakeAndLadderGame {
-	
+class Game{
 	//player at start position 0
-	static int position=0;
-	
-	 //Using Random to get a Random of a dice from 1 to 6 
-	static int diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
-	public static void main(String[] args) {
-		System.out.println("position of the player : " + position);
-		System.out.println("Random number of Dice is : " + diceNumber);
+		public static int position=0;
+		public static int diceNumber;
+		public static int moment;
+		public static final int ladder=1;
+		public static final int snake=2;
+		
+		//Using Random to get a Random of a dice from 1 to 6 
+		public void rollADie() {
+			diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
+	    	System.out.println("Die number is : "+diceNumber);
 
+		}
+		
+		//Checking for the position whether its a ladder, snake or no play
+		public void checkMoment() {
+		    moment=(int)(Math.floor(Math.random()*10)%3);
+		    switch(moment) {
+		    case ladder:
+		    	System.out.println("your are at ladder climb up ");
+		    	break;
+		    case snake:
+		    	System.out.println("Snake bites, move back to the tail ");
+		    	break;
+		    default:
+		    	System.out.println("Stay at the same position");
+		    }
+		}
+}
+
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		Game g=new Game();
+		System.out.println("position of the player : " + Game.position);
+		g.rollADie();
+		g.checkMoment();
 	}
 }
