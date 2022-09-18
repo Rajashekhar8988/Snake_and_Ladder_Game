aaeb81a81e75f630c5d17942f576f3ffd38f72e2
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-18 12:43:38 +0530

    Player Reaches winning position of 100

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -9,33 +9,38 @@ class Game{
 		public static final int snake=2;
 		
 		//Using Random to get a Random of a dice from 1 to 6 
-		public void rollADie() {
-			diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
-	    	System.out.println("Die number is : "+diceNumber);
-
-		}
-		
 		//Checking for the position whether its a ladder, snake or no play
+		//Repeat till the Player reaches the winning position 100.
 		public void checkMoment() {
+			while(position<100) {
+				diceNumber=(int)(Math.floor(Math.random()*10)%6+1);
+		    	System.out.println("Die number is : "+diceNumber);
 		    moment=(int)(Math.floor(Math.random()*10)%3);
 		    switch(moment) {
 		    case ladder:
-		    	System.out.println("your are at ladder climb up ");
+		    	position+=diceNumber;
+		    	System.out.println("your are at ladder climb up : " +diceNumber);
 		    	break;
 		    case snake:
-		    	System.out.println("Snake bites, move back to the tail ");
+		    	position-=diceNumber;
+		    	System.out.println("Snake bites, move back to the tail : " +diceNumber);
 		    	break;
 		    default:
 		    	System.out.println("Stay at the same position");
+		    } 
+		    
+		    //In case the player position moves below 0, then the player restarts from 0
+		    if(position<0)
+		    	position=0;
 		    }
+	    	System.out.println("position of player is : "+position);
+
 		}
-}
+		}
 
 public class SnakeAndLadderGame {
 	public static void main(String[] args) {
 		Game g=new Game();
-		System.out.println("position of the player : " + Game.position);
-		g.rollADie();
 		g.checkMoment();
 	}
 }
